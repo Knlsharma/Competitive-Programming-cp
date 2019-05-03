97b387be6c82978a1554c2f5dcef0cb749950a19
Kunal Sharma <knlsharma678@gmail.com>
2019-05-03 23:32:28 +0530

    Null pointer exception (#2)

--- Linked List/Doubly_LinkedList/Doubly_Linkedlist.java
+++ Linked List/Doubly_LinkedList/Doubly_Linkedlist.java
@@ -6,6 +6,7 @@ class Node
 int data;
 Node next;
 Node prev;
+
 }
  
  class Doubly_LinkedList
@@ -45,13 +46,13 @@ head=x;
  System.out.println();
  System.out.print("Reverse path is");
  
- while( n!=null)
+ /* while( n!=null)
 	 
 	 {
 		  System.out.print(" ," + n.data);
 		  n=n.prev;
 		 
-	 }
+	 } */
 	 
  }
  
@@ -175,6 +176,57 @@ for(int i=0; i < pos-1;i++)
  }
  
  
+ public static void sort()
+{
+    Node ptr ,pt2;
+    
+    for (ptr = head; ptr.next != null; ptr = ptr.next)
+    
+    {
+        for( pt2=ptr.next;pt2!=null;pt2=pt2.next)
+        
+        {
+            if(pt2.data < ptr.data)
+             {
+             int temp=ptr.data;
+             ptr.data=pt2.data;
+             pt2.data=temp;
+             }
+        }
+    }
+}
+ 
+		  static void reverseImplementation()
+		 
+		 {
+			 
+			Node i1 = head;
+			Node j1 = head;
+			
+		   
+		while(j1.next!=null)
+		{
+			
+			j1=j1.next;
+			
+		}	
+			 
+	 while(i1!=j1 && j1!=i1.prev)
+	 {
+	    int temp=i1.data;
+		i1.data=j1.data;
+		j1.data=temp;
+		j1=j1.next;
+		i1=i1.next;
+			 
+ }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
  
  public static void main(String args[])
  {
@@ -188,12 +240,17 @@ insertAtLast(6556);   //  653-> 6556
 insertAtLast(577);	 // 653-> 6556 -> 577
 insertAtPos(12,1); 
  insertAtPos(16,3); 
-  // deleteBeg();
+ /* // deleteBeg();
   //  deleteAtPos(1);
-   deleteAtLast(1);
-
+   deleteAtLast(1);*/
+ // sort();
+ 
+ reverseImplementation();
+ 
 
  showAll();
+ 
+ 
  
  
  
