e14763728395619c678f1d3ebb375ce7c120c4c2
kunal sharma <knlsharma678@gmail.com>
2019-05-19 22:46:46 +0530

    check
    Contains() needs improvemnets !

--- Tree/tree.java
+++ Tree/tree.java
@@ -1,95 +1,134 @@
-class treebt { 
-  
-    /* Class containing left and right child of current node and key value*/
-    class Node { 
-        int key; 
-        Node left, right; 
-  
-        public Node(int item) { 
-            key = item; 
-            left = right = null; 
-        } 
-    } 
-  
-    // Root of BST 
-    Node root; 
-  
-    // Constructor 
-    treebt() {  
-        root = null;  
-    } 
-  
-    // This method mainly calls insertRec() 
-    void insert(int key) { 
-       root = insertRec(root, key); 
-    } 
-      
-    /* A recursive function to insert a new key in BST */
-    Node insertRec(Node root, int key) { 
-  
-        /* If the tree is empty, return a new node */
-        if (root == null) { 
-            root = new Node(key); 
-            return root; 
-        } 
-  
-        /* Otherwise, recur down the tree */
-        if (key < root.key) 
-            root.left = insertRec(root.left, key); 
-        else if (key > root.key) 
-            root.right = insertRec(root.right, key); 
-  
-        /* return the (unchanged) node pointer */
-        return root; 
-    } 
-  
-    // This method mainly calls InorderRec() 
-    void inorder()  { 
-       inorderRec(root); 
-    } 
-  
-    // A utility function to do inorder traversal of BST 
-    void inorderRec(Node root) { 
-        if (root != null) { 
-            inorderRec(root.left); 
-            System.out.println(root.key); 
-            inorderRec(root.right); 
-        } 
-    } 
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class tree
+ {
+
+
+    public static void inOrder(Node root) {
+
+        if( root !=  null)
+
+        {  
+		inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+
+        }
+
+    }
 	
-	 void contains(int key) { 
-       root = check(root, key); 
-    } 
-	  
-	  static boolean check(Node root ,int key)
-	  
-	  {
-		  // to be need added			  
-		  
-		  
-	  }
-  
-    // Driver Program to test above functions 
-    public static void main(String[] args) { 
-        treebt tree = new treebt(); 
-  
-        /* Let us create following BST 
-              50 
-           /     \ 
-          30      70 
-         /  \    /  \ 
-       20   40  60   80 */
-        tree.insert(50); 
-        tree.insert(30); 
-        tree.insert(20); 
-        tree.insert(40); 
-        tree.insert(70); 
-        tree.insert(60); 
-        tree.insert(80); 
-  
-        // print inorder traversal of the BST 
-        tree.inorder(); 
+	  public static void preOrder(Node root) {
+
+        if( root !=  null)
+
+        {  
 		
-		tree.contains(80);
-    } 
-} 
+        System.out.print(root.data + " ");
+        inOrder(root.left);
+		inOrder(root.right);
+
+        }
+
+    }
+	
+	  public static void postOrder(Node root) {
+
+        if( root !=  null)
+
+        {  
+		inOrder(root.left);
+        inOrder(root.right);
+		System.out.print(root.data + " ");
+        
+
+        }
+
+    }
+	
+	public static boolean contains(Node root , int value) {
+		
+		if(root.data ==  data2) 
+            return true;
+	 	
+
+         else if (value < data)
+		 {
+			 if( root.left == null)
+			 {
+				 return false ;
+			 }
+			  else
+			  {
+				 return root.left = contains(root ,value);
+				  
+			  }  
+		 }
+		 
+		 
+		  else
+		  {
+			  if( root.right == null)
+			 {
+				 return false ;
+			 }
+			  else
+			  {
+				 return root.right = contains(root ,value);		  
+			  }
+		  
+	      }  		
+		
+		
+}
+	
+	
+	
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        inOrder(root);
+		System.out.println("----------");
+		preOrder(root);
+		System.out.println("----------");
+		postOrder(root);
+		System.out.println("----------");
+		contains(root , 56);
+		
+    }	
+}
