798f44182bd8b365ad0d2ddb2e62c92a0e79afe8
Knlsharma <knlsharma678@gmail.com>
2021-08-09 23:41:56 +0530

    remaining code

--- HashMapAndHeap.java
+++ HashMapAndHeap.java
@@ -31,6 +31,46 @@ class HashMapAndHeap {
 	}
 
 	// Get Common Elements - 1
+	HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+	for (int val : arr1) {
+		if (hm.containsKey(val)) {
+			int oldfreq = hm.get(val);
+			hm.put(val, oldfreq + 1);
+		} else {
+			hm.put(val, 1);
+		}
+	}
+
+	for (int val2 : arr2) {
+		if (hm.containsKey(val2)) {
+			System.out.println(val2);
+			hm.remove(val2);
+		}
+	}
+
+	// Get Common Elements - 2
+	HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+	for (int val : arr1) {
+		if (hm.containsKey(val)) {
+			int oldfreq = hm.get(val);
+			hm.put(val, oldfreq + 1);
+		} else {
+			hm.put(val, 1);
+		}
+	}
+
+	for (int val2 : arr2) {
+		if (hm.containsKey(val2) && hm.get(val2) > 0) {
+			System.out.println(val2);
+			int oldfreq = hm.get(val2);
+			hm.put(val2, oldfreq - 1);
+		}
+	}
+}
+
+}
 
 
 }
